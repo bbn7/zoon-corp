262b341038b49db3603fa7c6e039d1d1973dade7
bbn7 <bilal@Bilals-MacBook-Pro-16.local>
2025-12-12 14:14:56 +0300

    Fix GitHub Pages deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: true,
   trailingSlash: true,
+  basePath: '/zoon-corp',
+  assetPrefix: '/zoon-corp',
   images: {
     unoptimized: true
   }
